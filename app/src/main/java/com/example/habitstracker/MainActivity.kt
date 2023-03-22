f77266ae12c090daf0ae638ca4556eb836539aee
mimikamaxim <mimika.maxim@gmail.com>
2023-03-22 11:16:07 +0500

    add about and title fragment, add drawer

--- app/src/main/java/com/example/habitstracker/MainActivity.kt
+++ app/src/main/java/com/example/habitstracker/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.habitstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.core.view.forEach
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.drawerlayout.widget.DrawerLayout.DrawerListener
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.navOptions
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.onNavDestinationSelected
+import com.example.habitstracker.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater,null,false)
+//        drawerLayout = binding.drawerLayout//TODO q
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        val navView: NavigationView =
+            findViewById<NavigationView>(R.id.nav_view)
+        NavigationUI.setupWithNavController(navView, navController)
+//            binding.navView//TODO q
+//        navView.setNavigationItemSelectedListener{
+//            val id = it.itemId
+//            Log.i("TAG",it.toString())
+//            if (id == R.id.homeFragment)
+//                navController.navigate(R.id.homeFragment)
+//            if (id == R.id.aboutFragment)
+//                navController.navigate(R.id.aboutFragment)
+//            drawerLayout.close()
+//            true
+//        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
