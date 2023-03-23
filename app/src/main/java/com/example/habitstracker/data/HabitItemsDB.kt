6f2199f8662911dc6f9ab9621bc82f40296e8cb9
mimikamaxim <mimika.maxim@gmail.com>
2023-03-23 10:18:06 +0500

    fix fill DB in HabitItemsDB.kt

--- app/src/main/java/com/example/habitstracker/data/HabitItemsDB.kt
+++ app/src/main/java/com/example/habitstracker/data/HabitItemsDB.kt
@@ -56,11 +56,11 @@ object HabitItemsDB {
     }
 
     fun fillDBsample() {
-        habitItems.add(HabitItem("Читать новости", "Читать свежие новости", 5, false, 1, "Раз в день",Color.GRAY))
-        habitItems.add(HabitItem("Физические упражнения", "Заниматься физической нагрузкой в течении часа", 5, true, 1, "Раз в неделю",Color.RED))
-        habitItems.add(HabitItem("Магазин", "Ходить за продуктами", 3, true, 1, "Раз в неделю", Color.CYAN))
-        habitItems.add(HabitItem("Уборка", "Прибраться в комнате", 1, true, 1, "Раз в неделю", Color.MAGENTA))
-        habitItems.add(HabitItem("Ходить в бар","Ходить с друзьями в бар",3,false,2,"Раз в месяц",Color.YELLOW))
+        addHabit(HabitItem("Читать новости", "Читать свежие новости", 5, false, 1, "Раз в день",Color.GRAY))
+        addHabit(HabitItem("Физические упражнения", "Заниматься физической нагрузкой в течении часа", 5, true, 1, "Раз в неделю",Color.RED))
+        addHabit(HabitItem("Магазин", "Ходить за продуктами", 3, true, 1, "Раз в неделю", Color.CYAN))
+        addHabit(HabitItem("Уборка", "Прибраться в комнате", 1, true, 1, "Раз в неделю", Color.MAGENTA))
+        addHabit(HabitItem("Ходить в бар","Ходить с друзьями в бар",3,false,2,"Раз в месяц",Color.YELLOW))
     }
 }
 
